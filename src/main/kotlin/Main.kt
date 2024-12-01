4186d5ea0e8b96b971c53605c6647f43d65ab638
Alexander <alexbond.xxi@gmail.com>
2024-12-01 20:48:33 +0300

    Листинг 6.3 Другие примеры использования строковых шаблонов. В строки интерполирована переменная $HERO_NAME

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+const val HERO_NAME = "Madrigal"
+var playerLevel = 5
+fun main(){
+
+    println("$HERO_NAME announces her presence to the world.")
+
+    println(HERO_NAME)
+
+
+    println(playerLevel)
+
+    readBountyBoard()
+    println("Time passes...")
+    println("$HERO_NAME returns from her quest")
+    playerLevel +=1
+    println(playerLevel)
+    readBountyBoard()
+}
+private fun readBountyBoard(){
+    println("$HERO_NAME approaches in the bounty-board. It reads:")
+    println(obtainQuest(playerLevel))
+}
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String = "paladin",
+    hasBefriendedBarbarians: Boolean = true,
+    hasAngeredBarbarians: Boolean = false
+
+):String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 1..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+            // Проверить возможность дипломатического решения
+            if (canTalkToBarbarians) {
+                "Convince the baf their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+
+        }
+
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> "There are no quests right now."
+    }
